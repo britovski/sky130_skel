ed3146807f5f450d7e21e3f427e85868bdcd7262
britovski <brito.rn@gmail.com>
2021-01-13 23:25:17 -0300

    Relocating the script files to scripts directory

---
+++ scripts/minimal_opentools.sh
@@ -0,0 +1,77 @@
+echo "Installation of EDA open source tools and minimal configuration to use sky130 open PDK will begin..."
+
+#echo "You will need root permissions to perform tools installation..."
+
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+echo "Setting up directories..."
+
+cd /
+mkdir edatools 
+cd edatools
+#mkdir tools
+#cd tools
+mkdir opentools
+cd opentools
+
+echo "Solving dependencies..."
+yum update -y
+yum upgrade -y
+yum install gcc gcc-c++ autoconf automake patch patchutils indent libtool python3 cmake git -y
+yum install mesa-libGL mesa-libGLU mesa-libGLU-devel libXp libXp-devel libXmu-devel tcl tk tcl-devel tk-devel cairo cairo-devel -y
+yum install graphviz libXaw-devel readline-devel flex bison -y
+#yum install openmpi openmpi-devel openmpi3 openmpi-devel3 -y #use if you want multi-core support
+
+echo "Downloading tools..."
+wget -O ngspice-33.tar.gz https://sourceforge.net/projects/ngspice/files/ng-spice-rework/33/ngspice-33.tar.gz/download
+wget -O adms-2.3.6.tar.gz https://sourceforge.net/projects/mot-adms/files/adms-source/2.3/adms-2.3.6.tar.gz/download
+wget http://opencircuitdesign.com/magic/archive/magic-8.3.113.tgz
+wget http://opencircuitdesign.com/netgen/archive/netgen-1.5.162.tgz
+ 
+echo "Installing tools..."
+
+echo "Installing Ngspice with ADMS and XSPICE support..."
+tar zxvpf adms-2.3.6.tar.gz
+cd adms-2.3.6
+./configure
+make
+make install
+cd ..
+
+tar zxvpf ngspice-33.tar.gz
+cd ngspice-33
+wget -O ng_adms_va.tar.gz https://sourceforge.net/projects/ngspice/files/ng-spice-rework/33/ng_adms_va.tar.gz/download
+tar zxvpf ng_adms_va.tar.gz
+./autogen.sh --adms
+mkdir release
+cd release
+../configure --with-x --enable-xspice --disable-debug --enable-cider --with-readline=yes --enable-adms CFLAGS=-std=c99
+make -j$(nproc) CFLAGS=-std=c99
+make install
+#../configure --with-x --enable-xspice --enable-cider --enable-openmp --with-readlines=yes --disable-debug --enable-adms #use if you want multi-core support
+
+cd ../../
+
+echo "Installing Magic Layout..."
+tar zxvpf magic-8.3.113.tgz
+cd magic-8.3.113
+./configure
+make
+make install
+
+cd ..
+
+echo "Installing Netgen..."
+tar zxvpf netgen-1.5.162.tgz
+cd netgen-1.5.162
+./configure
+make
+make install
+
+echo "Minimal EDA open source tools installation done!"
+echo "Back to user and run the 'minimal_sky130_skel.sh' script"
+
---
+++ scripts/recommended_AMS_sky130_pdk_install.sh
@@ -0,0 +1,74 @@
+echo "Installation of sky130 open source PDK will begin..."
+
+echo "You will need root permissions to perform tools installation..."
+
+#if [[ $EUID -ne 0 ]]; then
+#   echo "This script must be run as root"
+#   exit 1
+#fi
+
+echo "Creating pdks directory..."
+
+cd /
+sudo mkdir edatools
+cd edatools
+#sudo mkdir tools
+#cd tools
+sudo mkdir pdks
+cd pdks
+
+export TOOLS_DIR=/edatools
+#export TOOLS_DIR=/tools
+
+echo "Cloning sky130 libraries..."
+sudo git clone https://github.com/google/skywater-pdk
+cd skywater-pdk
+#sudo git submodule init libraries/sky130_fd_io/latest
+sudo git submodule init libraries/sky130_fd_pr/latest
+sudo git submodule init libraries/sky130_fd_sc_hd/latest
+#sudo git submodule init libraries/sky130_fd_sc_hdll/latest
+#sudo git submodule init libraries/sky130_fd_sc_hs/latest
+#sudo git submodule init libraries/sky130_fd_sc_ms/latest
+#sudo git submodule init libraries/sky130_fd_sc_ls/latest
+#sudo git submodule init libraries/sky130_fd_sc_lp/latest
+#sudo git submodule init libraries/sky130_fd_sc_hvl/latest
+sudo git submodule update
+#sudo make timing #uncomment just for digital flow 
+
+cd ..
+
+#uncomment below for Open_PDKs compatibility
+#echo "Cloning Open_PDKs tool and setting up for tool flow compatibility..."
+#sudo git clone git://opencircuitdesign.com/open_pdks
+#cd open_pdks
+#sudo git checkout open_pdks-1.0
+#sudo ./configure --enable-sky130-pdk=$TOOLS_DIR/pdks/skywater-pdk --with-sky130-local-path=$TOOLS_DIR/pdks
+#cd sky130
+#sudo make
+#sudo make install
+
+echo "Creating sky130_skel..."
+cd ~
+mkdir sky130_skel
+cd sky130_skel
+ 
+echo "Preparing libraries for AMS flow using XSchem..."
+
+echo "Cloning XSchem_sky130 repo..."
+git clone https://github.com/StefanSchippers/xschem_sky130.git
+
+echo "Setting up sky130 libraries..."
+cd $TOOLS_DIR/pdks/skywater-pdk/libraries
+sudo cp -r sky130_fd_pr sky130_fd_pr_ngspice
+cd sky130_fd_pr_ngspice/latest
+sudo patch -p2 < ~/sky130_skel/xschem_sky130/sky130_fd_pr.patch
+
+echo "#####################################################################################################################"
+echo "To complete the workarea setup you need to update the last two lines of the file 'xschemrc' with the following paths:"
+echo "set SKYWATER_MODELS $TOOLS_DIR/pdks/skywater-pdk/libraries/sky130_fd_pr_ngspice/latest"
+echo "set SKYWATER_STDCELLS $TOOLS_DIR/pdks/skywater-pdk/libraries/sky130_fd_sc_hd/latest"
+echo "#####################################################################################################################"
+
+cd ~/sky130_skel/xschem_sky130
+
+echo "Recommended sky130 open source PDK installation for AMS flow done!"
